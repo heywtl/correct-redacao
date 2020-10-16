fdada8ba793cebf9d01392e2f5f253b0d07c78a3
Lincoln Souza <lincolnams4@gmail.com>
2020-10-16 00:46:58 -0300

    firebase adds

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
+import firebaseui from 'firebaseui';
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAk2G45MO-if6VYZk_kypt5dvTCNkPzoyw",
+  authDomain: "dsid-gp5.firebaseapp.com",
+  databaseURL: "https://dsid-gp5.firebaseio.com",
+  projectId: "dsid-gp5",
+  storageBucket: "dsid-gp5.appspot.com",
+  messagingSenderId: "1022571067612",
+  appId: "1:1022571067612:web:b4a1f9d227771120e4dc19",
+  measurementId: "G-BHVWHBKHCW"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
