445107c2787904443939fc59f941f09806608b4b
heitorfigueira <heitor.pfigueira@outlook.com>
2020-10-15 21:36:19 -0300

    configuração do vuetify e começo do login

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,92 @@
+<template>
+    <span>
+        <v-navigation-drawer
+            app
+            v-model="drawer"
+            class="blue lighten-2"
+            dark
+            disable-resize-watcher
+        >
+            <v-list>
+                <template v-for="(item, index) in items">
+                    <v-list-tile :key="index" :to="item.url">
+                        <v-list-tile-content>
+                            {{ item.title }}
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider :key="`divider-${index}`"></v-divider>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar app color="blue darken-4" dark>
+            <v-toolbar-side-icon
+                class="hidden-md-and-up"
+                @click="drawer = !drawer"
+            ></v-toolbar-side-icon>
+            <v-spacer class="hidden-md-and-up"></v-spacer>
+            <router-link to="/">
+                <v-toolbar-title data-cy="titleBtn">{{
+                    appTitle
+                }}</v-toolbar-title>
+            </router-link>
+            <v-btn
+                flat
+                class="hidden-sm-and-down nav-menu"
+                to="/menu"
+                data-cy="menuBtn"
+                >Menu</v-btn
+            >
+            <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <div v-if="!isAuthenticated" class="hidden-sm-and-down">
+                <v-btn flat to="/sign-in" data-cy="signinBtn">SIGN IN</v-btn>
+                <v-btn
+                    color="blue lighten-3"
+                    to="/join"
+                    class="nav-join"
+                    data-cy="joinBtn"
+                    >JOIN</v-btn
+                >
+            </div>
+            <div v-else>
+                <v-btn flat to="/about">PROFILE</v-btn>
+                <v-btn outline color="white" @click="logout" data-cy="logout"
+                    >Logout</v-btn
+                >
+            </div>
+        </v-toolbar>
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation',
+    data() {
+        return {
+            appTitle: 'CORRECTRedação',
+            drawer: false,
+            items: [
+                { title: 'Menu', url: '/menu' },
+                { title: 'Cadastro', url: '/cadastro' },
+                { title: 'Login', url: '/login' }
+            ]
+        };
+    },
+    computed: {
+        isAuthenticated() {
+            return true;
+        }
+    },
+    methods: {
+        logout() {
+            this.$store.dispatch('userSignOut');
+        }
+    }
+};
+</script>
+
+<style scoped>
+a {
+    color: white;
+    text-decoration: none;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import Cadastro from '../views/Cadastro.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/cadastro',
+    name: 'Cadastro',
+    component: Cadastro
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
     path: '/about',
@@ -21,7 +33,9 @@ const routes: Array<RouteConfig> = [
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  base: 'http://localhost:8080/',
+  mode: 'history'
 })
 
 export default router
