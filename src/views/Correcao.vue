636436c0e6020373f69bf1121aa922349f4bc4c9
Lincoln Souza <lincolnams4@gmail.com>
2020-11-20 21:15:18 -0300

    Update Correcao.vue

--- src/views/Correcao.vue
+++ src/views/Correcao.vue
@@ -1,62 +1,76 @@
 <template>
   <v-container>
-    <v-row align="center">
-      <v-col class="text-h4" align-self="center"> Corretor </v-col>
-    </v-row>
     <v-layout align-center justify-center>
-      <v-row align="center">
-        <v-col>
-          <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
-            <div>
-              <button
-                :class="{ 'is-active': isActive.bold() }"
-                @click="commands.bold"
-              >
-                <v-icon>mdi-format-bold</v-icon>
-              </button>
-              <button
-                :class="{ 'is-active': isActive.italic() }"
-                @click="commands.italic"
-              >
-                <v-icon>mdi-format-italic</v-icon>
-              </button>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Corrigir Redação</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form" v-model="valid">
+              <v-layout align-center justify-center>
+                <v-flex xs10 sm10 md10>
+                  <editor-menu-bar
+                    :editor="editor"
+                    v-slot="{ commands, isActive }"
+                  >
+                    <div>
+                      <button
+                        :class="{ 'is-active': isActive.bold() }"
+                        @click="commands.bold"
+                      >
+                        <v-icon>mdi-format-bold</v-icon>
+                      </button>
+                      <button
+                        :class="{ 'is-active': isActive.italic() }"
+                        @click="commands.italic"
+                      >
+                        <v-icon>mdi-format-italic</v-icon>
+                      </button>
 
-              <button
-                :class="{ 'is-active': isActive.strike() }"
-                @click="commands.strike"
-              >
-                <v-icon>mdi-format-strike</v-icon>
-              </button>
+                      <button
+                        :class="{ 'is-active': isActive.strike() }"
+                        @click="commands.strike"
+                      >
+                        <v-icon>mdi-format-strike</v-icon>
+                      </button>
 
-              <button
-                :class="{ 'is-active': isActive.underline() }"
-                @click="commands.underline"
-              >
-                <v-icon>mdi-format-underline</v-icon>
-              </button>
+                      <button
+                        :class="{ 'is-active': isActive.underline() }"
+                        @click="commands.underline"
+                      >
+                        <v-icon>mdi-format-underline</v-icon>
+                      </button>
 
-              <button
-                :class="{ 'is-active': isActive.code_block() }"
-                @click="commands.code_block"
-              >
-                <v-icon>mdi-comment-quote-outline</v-icon>
-              </button>
+                      <button
+                        :class="{ 'is-active': isActive.code_block() }"
+                        @click="commands.code_block"
+                      >
+                        <v-icon>mdi-comment-quote-outline</v-icon>
+                      </button>
 
-              <button @click="commands.horizontal_rule">
-                <v-icon>mdi-minus</v-icon>
-              </button>
+                      <button @click="commands.horizontal_rule">
+                        <v-icon>mdi-minus</v-icon>
+                      </button>
 
-              <button @click="commands.undo"><v-icon>mdi-undo</v-icon></button>
+                      <button @click="commands.undo">
+                        <v-icon>mdi-undo</v-icon>
+                      </button>
 
-              <button @click="commands.redo"><v-icon>mdi-redo</v-icon></button>
-            </div>
-          </editor-menu-bar>
-        </v-col>
-      </v-row>
-      <div></div>
-      <v-row align="center">
-        <v-col><editor-content :editor="editor" /></v-col>
-      </v-row>
+                      <button @click="commands.redo">
+                        <v-icon>mdi-redo</v-icon>
+                      </button>
+                    </div>
+                  </editor-menu-bar>
+                  <v-col align-self="center"
+                    ><editor-content :editor="editor"
+                  /></v-col>
+                </v-flex>
+              </v-layout>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
